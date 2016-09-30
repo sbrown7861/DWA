df4a9a3c383d60183c7fdbdd8fb2a1e68ae8cff9
sbrown7861 <sbrown14@fullsail.edu>
2016-09-29 19:30:24 -0700

    server.js
    server.js configured and running.

---
+++ Static_API/src/server.js
@@ -0,0 +1,19 @@
+
+// API dependant packages to be required
+
+var express = require('express');
+var body_parser = require('body-parser');
+
+// Instantiate "APP"
+
+var app = express();
+
+//Configuration
+
+var port = 3000;
+
+// Function to make sure server is up and running.
+app.listen(port,function(){
+    console.log('Server is active on', port)
+});
+
